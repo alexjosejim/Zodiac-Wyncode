c49547c73a56bf92f22c733a19390dbc97d0a682
vwilkins1 <vannessa.wilk@gmail.com>
2019-07-30 21:08:05 -0400

    minor styling changes

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -4,9 +4,13 @@ import {Link} from 'react-router-dom'
 const Footer = () =>{
     return (
           <footer>
-          <p>Posted by: Vannessa Wilkinson </p>
-      <p>Contact information: <a href="mailto:Vannessa.wilk@gmail.com">
-        Vannessa.wilk@gmail.com</a>.</p>
+          <p>Contact information: <a href="mailto:Vannessa.wilk@gmail.com">
+            Vannessa.wilk@gmail.com</a>.</p>
+          <ul>
+            <li><a href="#" class="social twitter">Twitter</a></li>
+            <li><a href="https://www.linkedin.com/in/vannessa-wilkinson-a5042b109/" target="_blank" class="social linkedin">LinkedIn</a></li>
+            <li><a href="https://github.com/vwilkins1" target="_blank" class="social github">Github</a></li>
+          </ul>
           </footer>
     )
   }
